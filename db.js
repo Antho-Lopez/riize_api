f28dc3271d192a940b0b2f641b9b7d93042b62e5
Lopez Anthony <70229078+Antho-Lopez@users.noreply.github.com>
2025-04-21 17:11:58 +0200

    change db connection params

--- db.js
+++ db.js
@@ -1,20 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config(); // Charger les variables d'environnement
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 // Vérifier la connexion
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
-        console.error('Erreur de connexion à la base de données:', err);
-        return;
+        console.error('❌ Erreur de connexion à la base de données :', err);
+    } else {
+        console.log('✅ Connexion MySQL réussie');
+        connection.release();
     }
-    console.log('Connecté à la base de données MySQL');
-});
+    });
 
 module.exports = db;
